e4f6ac0150f2412ac5d4fa2b9107adaf777a13aa
weeat <108050444+weeattt@users.noreply.github.com>
2022-09-08 10:43:30 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -12,10 +12,13 @@ public class Main {
             int j = Integer.parseInt(text[0]);
             double max=0;
             double sum=0;
+// double 은 8byte 실수, float 은 4byte 실수
+// 왜 구분하냐고 불평할 수 있지만 컴퓨터 구조에 대해 배우면 당연하다.
  
             String num[] = s_rd.readLine().split(" ");
             double re_num[] = new double[j];
             
+// re_num[i] 는 세준이의 변환될 성적            
 //            max 값 구하기
             for(int i=0; i<j; i++) {
                 re_num[i] = Integer.parseInt(num[i]);
